87e1a96d839aafba678155a7e38c55a2de380776
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2025-02-28 22:41:50 -0400

    chore: add router express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { router } from "./router";
 
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => {
   console.log("http://localhost:3333");
