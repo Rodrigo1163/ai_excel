e3d550137e2013b49652cf7828972069ebb1cbdc
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2025-03-04 12:17:16 -0400

    chore: config middleware multer

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+
+import multer from "multer";
+import uploadConfig from "./config/multer";
+
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { isAuthtenticated } from "./middlewares/isAuthtenticated";
 
 const router = Router();
+const upload = multer(uploadConfig.upload("./tmp/excel"));
 
 router.post("/users", isAuthtenticated, new CreateUserController().handle);
 router.get("/profile/:id", isAuthtenticated, new DetailUserController().handle);
